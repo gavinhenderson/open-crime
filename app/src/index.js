6420dab138964024f54f8feea25b9462f86ff1e3
gavinhenderson <gavin.henderson@hotmail.co.uk>
2018-08-02 09:32:57 +0100

    Initial commit of app

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './theme/semantic.less';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
