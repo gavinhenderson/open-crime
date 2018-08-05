a186ff6af55176ec44f95c8cce4109ac315f9d1d
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-08-05 17:02:58 +0100

    Setup apollo client

--- app/src/App.js
+++ app/src/App.js
@@ -3,10 +3,20 @@ import "styling/semantic.less";
 import React from "react";
 import { Button, Icon } from "semantic-ui-react";
 import { Home } from "./pages";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:8000/graphql"
+});
 
 class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <ApolloProvider client={client}>
+        <Home />
+      </ApolloProvider>
+    );
   }
 }
 
