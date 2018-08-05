ff2ae2538b42cba78c1a537c46843f42b2f11e19
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-08-05 17:02:44 +0100

    Now use CORS to allow requesting from app

--- api/index.js
+++ api/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
 const PORT = 8000;
-const { importSchema } = require('graphql-import');
-const { buildSchema } = require('graphql');
+const { importSchema } = require("graphql-import");
+const { buildSchema } = require("graphql");
 const createSchema = location => buildSchema(importSchema(location));
-const schema = createSchema('graphql/schema.graphql');
+const schema = createSchema("graphql/schema.graphql");
+const cors = require("cors");
 
 // root resolver
-const rootValue = require('./resolver');
+const rootValue = require("./resolver");
 
 const app = express();
+
+app.use(cors());
+
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema,
     rootValue,
@@ -20,7 +24,7 @@ app.use(
 );
 
 // Host docs
-app.use('/docs', express.static('docs'));
+app.use("/docs", express.static("docs"));
 
 app.listen(PORT, () => {
   console.log(`GraphQL running on port: ${PORT}`);
