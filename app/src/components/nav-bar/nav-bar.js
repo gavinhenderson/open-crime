0c049834e467ee4e68b851adc11cfa67202c0bf3
gavinhenderson <gavin.henderson@hotmail.co.uk>
2018-08-20 09:04:16 +0100

    Navbar now meets design

--- app/src/components/nav-bar/nav-bar.js
+++ app/src/components/nav-bar/nav-bar.js
@@ -6,6 +6,20 @@ class NavBar extends React.Component {
       <div className={'nav-bar'}>
         <div className={'nav-bar-contents'}>
           <h1 className={'nav-bar-header'}>Open-Crime</h1>
+          <div className={'nav-bar-buttons'}>
+            <a
+              href="/home"
+              className={'nav-bar-button nav-bar-button-selected'}
+            >
+              Home
+            </a>
+            <a href="/contact-us" className={'nav-bar-button'}>
+              Contact Us
+            </a>
+            <a href="/about" className={'nav-bar-button'}>
+              About
+            </a>
+          </div>
         </div>
       </div>
     );
