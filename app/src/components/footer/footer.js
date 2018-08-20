dd9e0e56b2f932625d37265df9080f99b9c613af
gavinhenderson <gavin.henderson@hotmail.co.uk>
2018-08-20 09:55:29 +0100

    Footer started

--- app/src/components/footer/footer.js
+++ app/src/components/footer/footer.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Footer = () => <p className={'footer-container'}>This is a test</p>;
+const Footer = () => (
+  <div className={'footer-container'}>
+    <div className={'footer-content'}>
+      <a href="">Data.Police.UK Public API</a>
+      <a href="">Github Project</a>
+      <a href="">Tech Blog {'</>'}</a>
+      <a href="">@gavinhenderson - Github</a>
+    </div>
+  </div>
+);
 
 export default Footer;
